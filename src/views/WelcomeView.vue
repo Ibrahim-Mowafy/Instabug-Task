d0f28f2f8f134070b2c9aceeb1fd9b8f7078b893
ibrahim <ibrahim.mowafy10@gmail.com>
2022-05-31 17:22:33 +0200

    Adding welcome form component

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <p class="container__greeting">
+      Welcome to
+      <span class="user">
+        {{ userData }}
+      </span>
+    </p>
+    <a class="container__btn" @click="onLogout">Logout</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WelcomeView",
+  data() {
+    return {
+      userData: localStorage.getItem("LoggedUser"),
+    };
+  },
+
+  methods: {
+    onLogout() {
+      localStorage.removeItem("LoggedUser");
+      this.$router.push("/login");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+  height: fit-content;
+  width: 100%;
+  margin-top: 5rem;
+  padding-inline: 2rem;
+
+  &__greeting {
+    font-size: 2rem;
+  }
+  .user {
+    color: #09c;
+    transition: color 0.3s;
+
+    &:hover {
+      color: #445065;
+    }
+  }
+
+  &__btn {
+    background-color: #f24220;
+    transition: 0.3s;
+    cursor: pointer;
+    outline: none;
+    border: transparent;
+    border-radius: 4px;
+    font-size: 1rem;
+    padding: 0.8rem;
+    color: #fff;
+    transition: background-color 0.3s;
+
+    &:hover {
+      background-color: #ff2a00;
+    }
+  }
+}
+</style>
